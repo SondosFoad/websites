84562993201e74123616056bdfd2547f50a1a334
SondosFoad <sondosfoadalnakhlani@gmail.com>
2025-12-07 08:24:05 +0300

    سي
    سي شارب

---
+++ Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace ClinicAppointmentSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.main = new System.Windows.Forms.Button();
+            this.connect = new System.Windows.Forms.Button();
+            this.reserve = new System.Windows.Forms.Button();
+            this.comment = new System.Windows.Forms.Button();
+            this.about = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(157)))), ((int)(((byte)(143)))));
+            this.panel1.Controls.Add(this.main);
+            this.panel1.Controls.Add(this.connect);
+            this.panel1.Controls.Add(this.reserve);
+            this.panel1.Controls.Add(this.comment);
+            this.panel1.Controls.Add(this.about);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(4, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1344, 222);
+            this.panel1.TabIndex = 0;
+            // 
+            // main
+            // 
+            this.main.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(157)))), ((int)(((byte)(143)))));
+            this.main.Location = new System.Drawing.Point(1035, 134);
+            this.main.Name = "main";
+            this.main.Size = new System.Drawing.Size(126, 43);
+            this.main.TabIndex = 6;
+            this.main.Text = "الصفحة الرئيسية";
+            this.main.UseVisualStyleBackColor = true;
+            this.main.Click += new System.EventHandler(this.main_Click);
+            // 
+            // connect
+            // 
+            this.connect.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(157)))), ((int)(((byte)(143)))));
+            this.connect.Location = new System.Drawing.Point(255, 134);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(126, 43);
+            this.connect.TabIndex = 5;
+            this.connect.Text = "تواصل معنا";
+            this.connect.UseVisualStyleBackColor = true;
+            this.connect.Click += new System.EventHandler(this.connect_Click);
+            // 
+            // reserve
+            // 
+            this.reserve.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(157)))), ((int)(((byte)(143)))));
+            this.reserve.Location = new System.Drawing.Point(456, 134);
+            this.reserve.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.reserve.Name = "reserve";
+            this.reserve.Size = new System.Drawing.Size(126, 43);
+            this.reserve.TabIndex = 4;
+            this.reserve.Text = "لحجز موعد";
+            this.reserve.UseVisualStyleBackColor = true;
+            this.reserve.Click += new System.EventHandler(this.reserve_Click);
+            // 
+            // comment
+            // 
+            this.comment.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(157)))), ((int)(((byte)(143)))));
+            this.comment.Location = new System.Drawing.Point(655, 134);
+            this.comment.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.comment.Name = "comment";
+            this.comment.Size = new System.Drawing.Size(126, 43);
+            this.comment.TabIndex = 3;
+            this.comment.Text = "التعليقات";
+            this.comment.UseVisualStyleBackColor = true;
+            this.comment.Click += new System.EventHandler(this.comment_Click);
+            // 
+            // about
+            // 
+            this.about.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(157)))), ((int)(((byte)(143)))));
+            this.about.Location = new System.Drawing.Point(836, 134);
+            this.about.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.about.Name = "about";
+            this.about.Size = new System.Drawing.Size(126, 43);
+            this.about.TabIndex = 2;
+            this.about.Text = "عن الموقع";
+            this.about.UseVisualStyleBackColor = true;
+            this.about.Click += new System.EventHandler(this.about_Click);
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(157)))), ((int)(((byte)(143)))));
+            this.button1.Location = new System.Drawing.Point(1656, 148);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 75);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "الصفحة الرئيسية";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(650, 43);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "عيادة الشفاء";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(590, 226);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(326, 41);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "مرحبًا بكم في عيادة الشفاء";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(478, 288);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(620, 27);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "نحن هنا لنقدم لكم أفضل رعاية صحية ممكنة بأحدث الوسائل وبأيدي أمهر الأطباء";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(571, 327);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(366, 27);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "هدفنا هو صحتك وراحتك، ونسعد دائمًا بخدمتكم";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::ClinicAppointmentSystem.Properties.Resources._68ec7b6d9352f70e0fa4e9080d81c18d;
+            this.pictureBox3.Location = new System.Drawing.Point(944, 410);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(323, 242);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ClinicAppointmentSystem.Properties.Resources.دكتور_اسنان;
+            this.pictureBox2.Location = new System.Drawing.Point(86, 410);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(423, 242);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ClinicAppointmentSystem.Properties.Resources._2051997_0;
+            this.pictureBox1.Location = new System.Drawing.Point(559, 410);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(323, 242);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1349, 734);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button reserve;
+        private System.Windows.Forms.Button comment;
+        private System.Windows.Forms.Button about;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button main;
+        private System.Windows.Forms.Button connect;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
+
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClinicAppointmentSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+            private void Form1_Load(object sender, EventArgs e)
+            {
+
+            }
+
+            private void label1_Click(object sender, EventArgs e)
+            {
+
+            }
+
+
+
+            private void label5_Click(object sender, EventArgs e)
+            {
+
+            }
+
+            private void comment_Click(object sender, EventArgs e)
+            {
+                Form3 f3 = new Form3();
+                f3.Show();
+                this.Hide();
+            }
+
+            private void main_Click(object sender, EventArgs e)
+            {
+                Form1 f1 = new Form1();
+                f1.Show();
+                this.Hide();
+            }
+
+            private void about_Click(object sender, EventArgs e)
+            {
+                Form2 f2 = new Form2();
+                f2.Show();
+                this.Hide();
+            }
+
+            private void reserve_Click(object sender, EventArgs e)
+            {
+                Form5 f5 = new Form5();
+                f5.Show();
+                this.Hide();
+            }
+
+            private void connect_Click(object sender, EventArgs e)
+            {
+                Form9 f9 = new Form9();
+                f9.Show();
+                this.Hide();
+            }
+        }
+    }
